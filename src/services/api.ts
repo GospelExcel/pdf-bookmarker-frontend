fec9516ca7f9f87a78120895719c072a7d6378b5
GospelExcel <gexcel123@gmail.com>
2025-11-21 00:27:22 -0500

    update API BASE URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://3.234.210.165:5001/api';
+const API_BASE_URL = 'https://api.booksmartai.app/api';
 
 export interface Document {
   id: number;
